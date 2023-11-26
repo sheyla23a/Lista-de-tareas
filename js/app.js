026a154a604aff38d097be2c0bdb7aa9dd672f84
sheyla astorga <sheylaastorga1998@gmail.com>
2023-11-25 21:31:14 -0300

    Agregue la funcionalidad de gestor de tareas: permite agregar y eliminar tareas dinámicamente.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+let totalTareas = 0;
 
+function agregarTarea() {
+  let tareaInput = document.getElementById("tareaInput");
+  let tarea = tareaInput.value;
+
+  if (!tarea) {
+    alert("Por favor, ingrese una tarea.");
+    return;
+  }
+
+  let listaTareas = document.getElementById("listaTareas");
+
+  let listItem = document.createElement("li");
+  listItem.className = "list-group-item";
+
+  let tareaText = document.createTextNode(tarea);
+  listItem.appendChild(tareaText);
+
+  let eliminarBtn = document.createElement("button");
+  eliminarBtn.className = "btn btn-danger btn-sm";
+  eliminarBtn.appendChild(document.createTextNode("Eliminar"));
+  eliminarBtn.onclick = function () {
+    listaTareas.removeChild(listItem);
+    actualizarTotalTareas();
+  };
+}
